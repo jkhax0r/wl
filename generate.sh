2114212ce55c0fe7afc6b8ae673cf851d99d121f
jk <>
2024-12-24 04:39:40 -0600

    update

--- generate.sh
+++ generate.sh
@@ -3,4 +3,5 @@
 ./dns_probe.sh plex.txt 1500 "i0.wp.com" "i1.wp.com" "www.opensubtitles.com" "secure.gravatar.com" "tmdb.org" "images.plex.tv" "plex.tv" "metadata-static.plex.tv" "metadata.provider.plex.tv" "plex.bz" "tvthemes.plexapp.com.cdn.cloudflare.net" "plexapp.com" "plex.services.com" "thetvdb.com" "themoviedb.com" "lencr.org" "plex.tv" "www.plex.tv" "pubsub.plex.bz"
 
 
+git commit -a -m "update"
 
