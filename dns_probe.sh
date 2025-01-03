cda8c14f2ec0a445f3259d93f94bd86bff246ca8
jk <>
2025-01-03 16:02:19 -0600

    update

--- dns_probe.sh
+++ dns_probe.sh
@@ -33,10 +33,11 @@ do
 	i=0	
 	echo "Processing $domain"
 	while read dns 
-	do 
-		BOGON_REGEX='^(0\.|127\.|10\.|192\.168\.|172\.16\.|172\.17\.|172\.18\.|172\.19\.|172\.2[0-9]\.|172\.3[0-1]\.|100\.64\.|169\.254\.|192\.0\.0\.|192\.0\.2\.|22[4-9]\.|23[0-9]\.|24[0-9]\.|25[0-5]\.)'
+	do	
+		IPV4_REGEX='[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'
+		BOGON_REGEX='(0\.|127\.|10\.|192\.168\.|172\.16\.|172\.17\.|172\.18\.|172\.19\.|172\.2[0-9]\.|172\.3[0-1]\.|100\.64\.|169\.254\.|192\.0\.0\.|192\.0\.2\.|22[4-9]\.|23[0-9]\.|24[0-9]\.|25[0-5]\.)'
 		
-		dig +short +time=2 @$dns $domain | grep '^[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+$' | grep -Ev "$BOGON_REGEX" >> data/$domain.txt &
+		dig +short +time=2 @$dns $domain | grep "^${IPV4_REGEX}$" | grep -Ev "^${BOGON_REGEX}" >> data/$domain.txt &
 
 		i=$((i+1))
 		if [[ $i == 1000 ]]
@@ -62,9 +63,12 @@ do
 	[[ $x -le 2 ]] && continue  #skip first 2 args
 
 	#Famous one-liner to store only unique lines in a file - http://www.catonmat.net/blog/awk-one-liners-explained-part-two/
-	awk '!a[$0]++' data/$domain.txt | sort | uniq | iprange > data/$domain.txt.tmp	
-	mv -f data/$domain.txt.tmp data/$domain.txt
+	awk '!a[$0]++' data/$domain.txt > data/$domain.txt.tmp
 
+	# Sort and remove BOGON networks, then compact into CIDR ranges using iprange utility 
+	cat data/$domain.txt.tmp | sort | uniq | rep -Ev "^${BOGON_REGEX}" | iprange > data/$domain.txt
+	rm -f data/$domain.txt.tmp	
+	
 	echo "#$domain" >> wl/$OUTPUT
 	cat data/$domain.txt >> wl/$OUTPUT
 	echo "" >> wl/$OUTPUT
