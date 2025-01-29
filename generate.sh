389bbe2dca2b5bdf39a5c23706a3d53be57a9adb
JK <jonathankaufmann@gmail.com>
2025-01-29 03:55:56 -0600

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -44,7 +44,8 @@ echo "142.250.0.0/15" >> wl/google.txt
 ./dns_probe.sh cloudflared.txt 1000 "region1.v2.argotunnel.com" "region2.v2.argotunnel.com" "_v2-origintunneld._tcp.argotunnel.com" "cftunnel.com" "h2.cftunnel.com" "quic.cftunnel.com"
 
 # All plex related
-./dns_probe.sh plex.txt 1000 "i0.wp.com" "i1.wp.com" "www.opensubtitles.com" "secure.gravatar.com" "tmdb.org" "images.plex.tv" "plex.tv" "metadata-static.plex.tv" "metadata.provider.plex.tv" "plex.bz" "tvthemes.plexapp.com.cdn.cloudflare.net" "plexapp.com" "plex.services.com" "thetvdb.com" "themoviedb.com" "lencr.org" "plex.tv" "www.plex.tv" "pubsub.plex.bz" "video.internetvideoarchive.net" "dlza6g8e6iucb.cloudfront.net" "m.media-amazon.com" "media-amazon.com" 
+./dns_probe.sh plex.txt 1000 "i0.wp.com" "i1.wp.com" "www.opensubtitles.com" "secure.gravatar.com" "images.plex.tv" "plex.tv" "metadata-static.plex.tv" "metadata.provider.plex.tv" "plex.bz" "tvthemes.plexapp.com.cdn.cloudflare.net" "plexapp.com" "plex.services.com" "themoviedb.com" "plex.tv" "www.plex.tv" "pubsub.plex.bz" "video.internetvideoarchive.net" 
+# Removed these 2, think it's mostly cloudfront... "m.media-amazon.com" "media-amazon.com" 
 
 
 ./dns_probe.sh synology.txt 1000 "global.download.synology.com" "global.synologydownload.com" "s3.us-west-002.backblazeb2.com" "update7.synology.com" "autoupdate7.synology.com" "payment.synology.com" "account.synology.com" "smtp.gmail.com" "download.synology.com" "www.synology.com" "pkgupdate7.synology.com" "pkgautoupdate.synologyupdate.com" "synoconf.synology.com" "synoconfkms.synology.com" "notification.synology.com" "sns.synology.com" "dataupdate.synology.com" "synology.com" "dataupdate7.synology.com" "dataautoupdate7.synology.com" "database.clamav.net" "myds.synology.com" "update.nai.com" "update.synology.com" "help.synology.com" "utyautoupdate.synology.com" "utyupdate.synology.com" "synosurveillance.synology.com" "us.c2.synology.com" "checkip.synology.com" "checkip.dyndns.org"
